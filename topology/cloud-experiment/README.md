68b7cee548fa4a5d6a31030a2bd6c54bbbdbd358
vsoch <vsoch@users.noreply.github.com>
2024-11-27 21:50:38 -0700

    experiment: cloud run for topologies, size 30 nodes 2GB
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ topology/cloud-experiment/README.md
@@ -0,0 +1,113 @@
+# Topology Experiments
+
+> Run on Google Cloud nodes
+
+Let's test different depths (N-ary) of trees. We first want to understand the structures of trees that flux generates depending on the topology spec and number of nodes. Then, for each depth we will test:
+
+1. Distribution from the root to all leaves (lowest level) 
+2. Distribution from the root to all nodes (regardless of level)
+3. Distribution from root to middle level, and then to leaves.
+
+We would want to see if there is a more efficient strategy, and then we would want to be able to combine Flux, a snapshotter, and possibly a CSI to distribute large files in Kubernetes. The idea would be that:
+
+1. The snapshotter (rank 0) would retrieve from the registry
+2. Rank 0 would distribute to workers
+3. The other ranks would have a CSI to bind to the node.
+
+We could also JUST use a snapshotter OR the CSI.
+
+
+## Flux Trees
+
+Note that the flux-design* files were generated in the [kind-experiment](../kind-experiment) and you will need them here.
+
+## Usage
+
+Run the experiments! First we will do a max size of 2 on 6 nodes.
+
+```bash
+time gcloud container clusters create test-cluster \
+    --threads-per-core=1 \
+    --num-nodes=6 \
+    --machine-type=c2d-standard-32 \
+    --enable-gvnic \
+    --network=mtu9k \
+    --placement-type=COMPACT \
+    --region=us-central1-a \
+    --project=${GOOGLE_PROJECT} 
+
+kubectl apply -f https://raw.githubusercontent.com/flux-framework/flux-operator/refs/heads/main/examples/dist/flux-operator.yaml
+
+python run-experiment.py --data ./kary-designs.json --max-nodes=6 --max-size=2 --data-dir ./data/raw-max-6
+time gcloud container clusters delete test-cluster --region=us-central1-a
+python run-analysis.py --out ./data/parsed-max-6 --data ./data/raw-max-6
+```
+```
+Experiments are done!
+total time to run is 5793.205878019333 seconds
+```
+
+Next, let's just test a large size (30)
+
+```bash
+6:40
+time gcloud container clusters create test-cluster \
+    --threads-per-core=1 \
+    --num-nodes=30 \
+    --machine-type=c2d-standard-32 \
+    --enable-gvnic \
+    --network=mtu9k \
+    --placement-type=COMPACT \
+    --region=us-central1-a \
+    --project=${GOOGLE_PROJECT} 
+
+kubectl apply -f https://raw.githubusercontent.com/flux-framework/flux-operator/refs/heads/main/examples/dist/flux-operator.yaml
+
+python run-experiment.py --data ./kary-designs.json --max-size=2 --exact-nodes=30 --data-dir ./data/raw-exact-30
+time gcloud container clusters delete test-cluster --region=us-central1-a
+python run-analysis.py --out ./data/parsed-exact-30 --data ./data/raw-exact-30
+```
+```console
+Experiments are done!
+total time to run is 9767.250812530518 seconds
+```
+
+## Results
+
+### Google Cloud 6 Nodes
+
+Let's look at 6 "nodes"
+
+![data/parsed-max-6/clean-all-nodes-nodes-6.png](data/parsed-max-6/clean-all-nodes-nodes-6.png)
+![data/parsed-max-6/clean-leaf-nodes-nodes-6.png](data/parsed-max-6/clean-leaf-nodes-nodes-6.png)
+![data/parsed-max-6/clean-middle-nodes-nodes-6.png](data/parsed-max-6/clean-middle-nodes-nodes-6.png)
+![data/parsed-max-6/create-archive-nodes-6.png](data/parsed-max-6/create-archive-nodes-6.png)
+![data/parsed-max-6/create-leaf-nodes-nodes-6.png](data/parsed-max-6/create-leaf-nodes-nodes-6.png)
+![data/parsed-max-6/create-middle-nodes-nodes-6.png](data/parsed-max-6/create-middle-nodes-nodes-6.png)
+![data/parsed-max-6/delete-archive-all-nodes-nodes-6.png](data/parsed-max-6/delete-archive-all-nodes-nodes-6.png)
+![data/parsed-max-6/delete-archive-leaf-nodes-nodes-6.png](data/parsed-max-6/delete-archive-leaf-nodes-nodes-6.png)
+![data/parsed-max-6/delete-archive-middle-nodes-nodes-6.png](data/parsed-max-6/delete-archive-middle-nodes-nodes-6.png)
+![data/parsed-max-6/distribute-all-nodes-nodes-6.png](data/parsed-max-6/distribute-all-nodes-nodes-6.png)
+![data/parsed-max-6/distribute-leaf-nodes-nodes-6.png](data/parsed-max-6/distribute-leaf-nodes-nodes-6.png)
+![data/parsed-max-6/distribute-middle-nodes-0-nodes-6.png](data/parsed-max-6/distribute-middle-nodes-0-nodes-6.png)
+![data/parsed-max-6/distribute-middle-nodes-1-nodes-6.png](data/parsed-max-6/distribute-middle-nodes-1-nodes-6.png)
+
+### Google Cloud 30 Nodes
+
+This will allow for more "kary" designs.
+
+![data/parsed-exact-30/clean-all-nodes-nodes-6.png](data/parsed-exact-30/clean-all-nodes-nodes-6.png)
+![data/parsed-exact-30/clean-leaf-nodes-nodes-6.png](data/parsed-exact-30/clean-leaf-nodes-nodes-6.png)
+![data/parsed-exact-30/clean-middle-nodes-nodes-6.png](data/parsed-exact-30/clean-middle-nodes-nodes-6.png)
+![data/parsed-exact-30/create-archive-nodes-6.png](data/parsed-exact-30/create-archive-nodes-6.png)
+![data/parsed-exact-30/create-leaf-nodes-nodes-6.png](data/parsed-exact-30/create-leaf-nodes-nodes-6.png)
+![data/parsed-exact-30/create-middle-nodes-nodes-6.png](data/parsed-exact-30/create-middle-nodes-nodes-6.png)
+![data/parsed-exact-30/delete-archive-all-nodes-nodes-6.png](data/parsed-exact-30/delete-archive-all-nodes-nodes-6.png)
+![data/parsed-exact-30/delete-archive-leaf-nodes-nodes-6.png](data/parsed-exact-30/delete-archive-leaf-nodes-nodes-6.png)
+![data/parsed-exact-30/delete-archive-middle-nodes-nodes-6.png](data/parsed-exact-30/delete-archive-middle-nodes-nodes-6.png)
+![data/parsed-exact-30/distribute-all-nodes-nodes-6.png](data/parsed-exact-30/distribute-all-nodes-nodes-6.png)
+![data/parsed-exact-30/distribute-leaf-nodes-nodes-6.png](data/parsed-exact-30/distribute-leaf-nodes-nodes-6.png)
+![data/parsed-exact-30/distribute-middle-nodes-0-nodes-6.png](data/parsed-exact-30/distribute-middle-nodes-0-nodes-6.png)
+![data/parsed-exact-30/distribute-middle-nodes-1-nodes-6.png](data/parsed-exact-30/distribute-middle-nodes-1-nodes-6.png)
+
+
---
+++ topology/cloud-experiment/run-experiment.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import time
+import sys
+from jinja2 import Template
+
+from kubernetes import client, config
+
+config.load_kube_config()
+
+# import the script we have two levels up
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(filename):
+    with open(filename, "r") as fd:
+        content = fd.read()
+    return content
+
+
+def write_file(content, filename):
+    with open(filename, "w") as fd:
+        fd.write(content)
+
+
+minicluster_template = read_file(os.path.join(here, "minicluster.yaml"))
+template = Template(minicluster_template)
+
+# Max number of nodes for m-ary tree with k levels
+# n = (mk − 1) / (m − 1)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Topology Experiment Running",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--name",
+        help="name for minicluster (defaults to flux-sample)",
+        default="flux-sample",
+    )
+    parser.add_argument(
+        "--data-dir",
+        help="path to save data",
+        default=os.path.join(here, "data", "raw"),
+    )
+    parser.add_argument(
+        "--max-size",
+        help="Maximum size to transfer",
+        default=10,
+        type=int,
+    )
+    parser.add_argument(
+        "--exact-nodes",
+        help="Exact node count to use",
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "--max-nodes",
+        help="Maximum nodes from design to use",
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "--iters",
+        help="Iterations per experiment",
+        default=5,
+        type=int,
+    )
+    parser.add_argument(
+        "--data",
+        help="path to kary designs json",
+        default=os.path.join(here, "kary-designs.json"),
+    )
+    return parser
+
+
+def read_json(filename):
+    """
+    Read json from file.
+    """
+    with open(filename, "r") as fd:
+        content = json.loads(fd.read())
+    return content
+
+
+def confirm_action(question):
+    """
+    Ask for confirmation of an action
+    """
+    response = input(question + " (yes/no)? ")
+    while len(response) < 1 or response[0].lower().strip() not in "ynyesno":
+        response = input("Please answer yes or no: ")
+    if response[0].lower().strip() in "no":
+        return False
+    return True
+
+
+def generate_uid(params):
+    """
+    Generate a unique id based on params.
+    """
+    uid = ""
+    for k, v in params.items():
+        if not isinstance(v, dict):
+            uid += k.lower() + "-" + str(v).lower()
+        else:
+            uid += k.lower()
+    return uid
+
+
+def write_json(obj, filename):
+    """
+    write json to output file
+    """
+    with open(filename, "w") as fd:
+        fd.write(json.dumps(obj, indent=4))
+
+
+def run_topology_experiment(exp, args, nodes_dir):
+    """
+    Run the topology experiment.
+    """
+    nodes = exp["nodes"]
+    render = {
+        "nodes": nodes,
+        "topo": exp["topo"],
+        "name": args.name,
+        "max_size": args.max_size,
+    }
+    outfile = os.path.join(nodes_dir, "topology-experiment.out")
+    if os.path.exists(outfile):
+        print(f"{outfile} exists, skipping")
+        return False
+    print(f"\n🍔 Running topology experiment size {nodes}")
+    levels = exp["levels"]
+
+    # Get the last level - where the leaves are
+    all_levels = list(levels)
+    all_levels.sort()
+    deepest_level = all_levels[-1]
+
+    # This will be lower half
+    middle_level = str(int(len(all_levels) / 2))
+
+    # For each level, first derive testing commands to send data from rank0 to (leaves)
+    render.update(
+        {
+            "leaf_nodes": ",".join(levels[deepest_level]),
+            "middle_nodes": ",".join(levels[middle_level]),
+        }
+    )
+    render = template.render(**render)
+    minicluster_yaml = os.path.join(nodes_dir, "minicluster.yaml")
+    write_file(render, minicluster_yaml)
+    run_kubectl(f"apply -f {minicluster_yaml}")
+    time.sleep(10)
+
+    # This will wait for it to finish
+    run_kubectl(f"wait --for=condition=complete --timeout=600s job/{args.name}")
+
+    cli = client.CoreV1Api()
+    for pod in cli.list_namespaced_pod(namespace="default").items:
+        if f"{args.name}-0" in pod.metadata.name:
+            log = cli.read_namespaced_pod_log(
+                name=pod.metadata.name, namespace="default"
+            )
+            break
+
+    if "Segmentation fault" in log or "Segment" in log:
+        print(f"Possible segmentation fault for {exp}!")
+
+    # Save to file
+    print(f"Writing topology log and recordings to {outfile}")
+    write_file(log, outfile)
+
+    # Wait for all pods to terminate
+    run_kubectl(f"delete -f {minicluster_yaml} --wait=true", allow_fail=True)
+    return True
+
+
+def run_kubectl(command, allow_fail=False):
+    """
+    Wrapper to client to run command with kubeconfig file
+    """
+    command = f"kubectl {command}"
+    res = os.system(command)
+    if res != 0 and not allow_fail:
+        print(
+            f"ERROR: running {command} - debug and ensure it works before exiting from session."
+        )
+        import IPython
+
+        IPython.embed()
+    return res
+
+
+def run_experiments(args, plans):
+    """
+    Generate runs for topology sizes
+    """
+    # Note that the experiment already has a table of values filtered down
+    # Each experiment has some number of batches (we will typically just run one experiment)
+    total = len(plans)
+    count = 0
+    for i, exp in enumerate(plans):
+        print(f"== Running experiment {exp}: {i} of {total}")
+
+        # Save the entire table just once
+        topo = exp["topo"].replace(":", "-")
+        nodes = exp["nodes"]
+        if args.exact_nodes is not None and nodes != args.exact_nodes:
+            continue
+        elif args.max_nodes is not None and nodes > args.max_nodes:
+            continue
+
+        for ii in range(args.iters):
+            path = os.path.join(args.data_dir, str(nodes), topo, str(ii))
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            # EXPERIMENTS: ---
+            # Run LAMMPS a number of iterations in the cluster
+            # This writes all iteration runs to one output file
+            try:
+                if run_topology_experiment(exp, args, path):
+                    count += 1
+            except:
+                print(f"Issue with {exp} - investigate!")
+
+    print("Experiments are done!")
+
+
+def main():
+    """
+    Run experiments for lammps, and collect hwloc info.
+    """
+    parser = get_parser()
+
+    # If an error occurs while parsing the arguments, the interpreter will exit with value 2
+    args, _ = parser.parse_known_args()
+    if not os.path.exists(args.data_dir):
+        os.makedirs(args.data_dir)
+
+    # Read in kary designs
+    if not os.path.exists(args.data):
+        sys.exit(f"{args.data} does not exist.")
+    plans = read_json(args.data)
+
+    # plan experiments!
+    print("🧪️ Experiments:")
+    print("🪴️ Planning to run:")
+    print(f"   Output Data         : {args.data_dir}")
+    print(f"   Experiments         : {len(plans)}")
+    print(f"      Max Size         : {args.max_size}")
+    print(f"         Iters         : {args.iters}")
+    if not confirm_action("Would you like to continue?"):
+        sys.exit("📺️ Cancelled!")
+
+    # Main experiment running, show total time just for user FYI
+    start_experiments = time.time()
+    run_experiments(args, plans)
+    stop_experiments = time.time()
+    total = stop_experiments - start_experiments
+    print(f"total time to run is {total} seconds")
+
+
+if __name__ == "__main__":
+    main()
